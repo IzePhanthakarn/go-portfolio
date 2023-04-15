b6795469f91c22227ee2e42dfae17b21e41d1de8
izephanthakarn <izephanthakarn@hotmail.com>
2023-04-16 01:29:48 +0700

    ✅ Complete employment API

--- route.go
+++ route.go
@@ -10,5 +10,8 @@ func serveRoutes(r *gin.Engine) {
 	employmentController := controller.Employment{}
 	employmentGroup := r.Group("/employments")
 	employmentGroup.GET("", employmentController.FindAll)
+	employmentGroup.GET("/:id", employmentController.FindOne)
 	employmentGroup.POST("", employmentController.Create)
+	employmentGroup.PATCH("/:id", employmentController.Update)
+	employmentGroup.DELETE("/:id", employmentController.Delete)
 }
